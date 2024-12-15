c7cdeb6834957268465431898ef7dc4824d63735
Valeriya <renery.kolins@gmail.com>
2024-12-15 20:44:10 +0100

    Completed class Menu according to the diagramm.

--- teamwork.py
+++ teamwork.py
@@ -1,14 +1,31 @@
-'''--- Menu ---'''
-
 import pandas
 import json
 
-df = pandas.read_excel('food.xlsx') # Be careful with the file path!
+class Menu():
 
-menu = {}
-for typ, group_typ in df.groupby("typ"):
-    menu[typ] = {}
-    for category, group_category in group_typ.groupby("categorie"):
-        menu[typ][category] = group_category.set_index("name")["price"].to_dict()
+    def __init__(self):
+        self.menu = {}
 
-print((json.dumps(menu, indent=4)))
+        df = pandas.read_excel('food.xlsx')  # Be careful with the file path!
+        for typ, group_typ in df.groupby("typ"):
+            self.menu[typ] = {}
+            for category, group_category in group_typ.groupby("categorie"):
+                self.menu[typ][category] = group_category.set_index("name")[
+                    "price"].to_dict()
+
+        print((json.dumps(self.menu, indent=4)))
+
+    def take_position(self, menu, position, amount):
+
+        for key, value in menu.items():
+            if key == position:
+                print(f"You've ordered {amount} {key} for {value} €.")
+                return {key: value, 'amount': amount}
+            elif isinstance(value, dict):
+                result = self.take_position(value, position, amount)
+                if result: return result
+        '''If return None, ask for another position in the next function.'''
+        return None
+
+test = Menu()
+test.take_position(test.menu, 'FOREST-BURGER', 1)
