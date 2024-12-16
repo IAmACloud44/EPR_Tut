f326ccfac45a36cddad99e9324f9b4b47a0a589d
IAmACloud44 <155573599+IAmACloud44@users.noreply.github.com>
2024-12-16 16:42:27 +0100

    I finished the seat assignments, I will add seat removal when you has orders ready :3

--- execute.py
+++ execute.py
@@ -1,29 +1,67 @@
 from teamwork import Table, tablers
 
+# execute the program so it works
 def excecute():
-    input_people = input("Please input how many people shall be seated: ")
+    '''
+    The function to execute the entire program.
+    
+    program designed so that it manages orders and creates invoices. 
+    It should be possible to view each table individually. Orders should be taken per table. 
+    The products ordered should be noted for each table. Of course, there can be repeat orders.
+    
+    An invoice should be able to be created at the end. In order to be able to resolve discrepancies, 
+    it must be possible to cancel or add individual items up until the invoice is paid.
+    
+    >>> input_people(5)
+    >>> Table 1: 8 seats, 0 taken seats
+    >>> Table 2: 6 seats, 0 taken seats
+    >>> Table 3: 6 seats, 0 taken seats
+    >>> input_table(3)
+    >>> Table 3: 6 seats, 5 taken seats
+    '''
+    # first asks how many people will arrive
+    input_people = input("Please input how many people shall be seated: ") 
 
+    # if int, proceed, if not, reaffirm that it is a number
     try:    
         input_people = int(input_people)
     except:
         print("Not a valid input")
         return
 
-    available = [table for table in tablers.values() if table.seats >= input_people and table.seats - table.taken >= input_people]
-    print(str(available))
+    # there are no tables with more than 8 seats
+    if input_people > 8:
+        print("Too many people, please seperate them in smaller groups")
+        return
 
-    input_table = input("Please input where you want to seat them: ")
+    # show newly loaded list with available seating
+    available = []
+    for table in tablers:
+        if table.seats - table.taken >= input_people:
+            table.show_table()
+            available.append(table)
 
-    # if input_table not in available:
-    #     print("That Table is not available.")
-    #     return
+    # ask for table
+    input_table = int(input("Please input where you want to seat them: "))
 
-    if input_people > tablers[input_table].seats or input_people > tablers[input_table].seats - tablers[input_table].taken:
-        print("Not enough seats.")
+    # ensures that the number put in is valid
+    selected_table = next((table for table in tablers if table.table_number == input_table), None)
+
+    # check wether the selected table is even available
+    if selected_table not in available:
+        print(f"Table {input_table} is not available.")
+        return
+
+    # notif of a table being full
+    if selected_table.seats == selected_table.taken:
+        print(f"Table {selected_table} is full!")
+
+    # add the people to the selected table
     else:
-        tablers[input_table].taken_seat(input_people)
-        tablers[input_table].show_table()
+        selected_table.taken_seat(input_people)
+        selected_table.show_table()
 
 
+# keeps the code running till fatal error occurs
 while True:
     excecute()
--- teamwork.py
+++ teamwork.py
@@ -4,7 +4,6 @@ import pandas
 import json
 
 
-# Menu
 class Menu():
 
     def __init__(self):
@@ -31,38 +30,39 @@ class Menu():
         '''If return None, ask for another position in the next function.'''
         return None
 
-test = Menu()
-test.take_position(test.menu, 'FOREST-BURGER', 1)
+# test = Menu()
+# test.take_position(test.menu, 'FOREST-BURGER', 1)
 
 
 # Restaurant Table arrangement
 class Table:
     def __init__(self, table_number, seats):
-        self.table_number = table_number
-        self.seats = seats
-        self.taken = 0
+        self.table_number = table_number  # Table numbers
+        self.seats = seats  # Total Seats
+        self.taken = 0  # Initial seat that are taken
 
     def __str__(self):
-        return self.table_number, self.seats, self.taken
+        return f"Table {self.table_number}"
 
+    # show the text nicer
     def show_table(self):
-        print(f"{self.table_number},{self.seats},{self.taken}")
+        print(f"Table {self.table_number}: {self.seats} seats, {self.taken} taken seats")
     
     def taken_seat(self, taken):
-        self.taken += taken
+        self.taken += taken  # Update the taken seats
 
 
-tablers = {
-    "Table1" : Table("Table1", 8),
-    "Table2" : Table(2, 6),
-    "Table3" : Table(3, 6),
-    "Table4" : Table(4, 4),
-    "Table5" : Table(5, 4),
-    "Table6" : Table(6, 4),
-    "Table7" : Table(7, 2),
-    "Table8" : Table(8, 2),
-    "Table9" : Table(9, 2),
-    "Table10" : Table(10, 2),
-    "Table11" : Table(11, 2),
-    "Table12" : Table(12, 2)
-}
+tablers = [
+    Table(1, 8),
+    Table(2, 6),
+    Table(3, 6),
+    Table(4, 4),
+    Table(5, 4),
+    Table(6, 4),
+    Table(7, 2),
+    Table(8, 2),
+    Table(9, 2),
+    Table(10, 2),
+    Table(11, 2),
+    Table(12, 2)
+]
